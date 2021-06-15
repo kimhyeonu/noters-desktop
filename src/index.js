f92a466cb2dc2cbe2bc2c32763cc9c02ac02b328
kimhyeonu <polaris@byeolmuri.com>
2021-06-15 18:00:56 +0900

    CSP 보안 설정

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 const { app, BrowserWindow } = require('electron');
-const { is } = require('electron-util');
+const { is, setContentSecurityPolicy } = require('electron-util');
+
+const config = require('./config');
 
 // 가비지 컬렉션을 피하기 위해 윈도우 객체를 변수로 선언한다.
 let window;
 
 function createWindow() {
   window = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
     },
   });
 
-  window.loadFile('index.html');
+  if (is.development) {
+    window.loadURL(config.LOCAL_WEB_URL);
+  } else {
+    window.loadURL(config.PRODUCTION_WEB_URL);
+  }
 
   if (is.development) {
     window.webContents.openDevTools();
+  }
+
+  if (!is.development) {
+    setContentSecurityPolicy(`
+    default-src 'none';
+    script-src 'self';
+    img-src 'self' https://www.gravatar.com;
+    style-src 'self' 'unsafe-inline';
+    font-src 'self';
+    connect-src 'self' ${config.PRODUCTION_API_URL};
+    base-uri 'none';
+    form-action 'none';
+    frame-ancestors 'none';
+  `);
   }
 
   window.on('closed', () => {
